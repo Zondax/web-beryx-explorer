2d95ed1cbcf3ca34d8a13e192e291eaee8ab20a3
Maria Ayelen Murano <muranoayelen@gmail.com>
2024-11-06 12:04:47 -0300

    upgrade eslint from scratch

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,121 +1,92 @@
-import prettier from 'eslint-plugin-prettier'
-import react from 'eslint-plugin-react'
-import globals from 'globals'
-import path from 'node:path'
-import { fileURLToPath } from 'node:url'
+import { fixupConfigRules, fixupPluginRules } from "@eslint/compat";
+import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+import promise from "eslint-plugin-promise";
+import react from "eslint-plugin-react";
+import tsdoc from "eslint-plugin-tsdoc";
+import unusedImports from "eslint-plugin-unused-imports";
+import globals from "globals";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 
-import { fixupConfigRules, fixupPluginRules } from '@eslint/compat'
-import { FlatCompat } from '@eslint/eslintrc'
-import js from '@eslint/js'
-import typescriptEslint from '@typescript-eslint/eslint-plugin'
-import tsParser from '@typescript-eslint/parser'
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
-})
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
 
-export default [
-  ...fixupConfigRules(
-    compat.extends(
-      'eslint:recommended',
-      'plugin:react/recommended',
-      'plugin:react-hooks/recommended',
-      'plugin:@typescript-eslint/recommended',
-      'prettier'
-    )
-  ),
-  {
-    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
-    ignores: ['**/.next/**', '**/dist/**', '.github', 'node_modules', 'public', '.ladle'],
-  },
-  {
+export default [{
+    ignores: ["**/.next", "**/.git", "**/node_modules", "**/public"],
+}, ...fixupConfigRules(compat.extends("next/core-web-vitals", "prettier", "turbo",
+      'plugin:react/recommended','eslint:recommended', 'plugin:@typescript-eslint/recommended')), {
     plugins: {
-      '@typescript-eslint': fixupPluginRules(typescriptEslint),
-      react: fixupPluginRules(react),
-      prettier,
+        "unused-imports": unusedImports,
+        '@typescript-eslint': fixupPluginRules(typescriptEslint),
+        react: fixupPluginRules(react),
+        tsdoc,
+        promise,
     },
 
     languageOptions: {
-      globals: {
-        ...globals.browser,
-        ...globals.node,
-        ...globals.jest,
-        Atomics: 'readonly',
-        SharedArrayBuffer: 'readonly',
-      },
-
-      parser: tsParser,
-      ecmaVersion: 'latest',
-      sourceType: 'module',
-
-      parserOptions: {
-        ecmaFeatures: {
-          jsx: true,
+        globals: {
+            ...globals.browser,
         },
-      },
+
+        parser: tsParser,
     },
 
     settings: {
-      react: {
-        version: 'detect',
-      },
+        react: {
+            version: "detect",
+        },
     },
 
     rules: {
-      '@typescript-eslint/explicit-function-return-type': 'off',
-      '@typescript-eslint/explicit-module-boundary-types': 'off',
-      '@typescript-eslint/no-explicit-any': 'off',
-      'prettier/prettier': 1,
-      '@typescript-eslint/ban-ts-comment': 'off',
-      '@typescript-eslint/no-unused-vars': 'off',
-      '@typescript-eslint/no-unused-expressions': 'off',
-      '@typescript-eslint/no-var-requires': 'off',
-      'no-loss-of-precision': 'off',
-      '@next/next/no-img-element': 'off',
-      'react-hooks/exhaustive-deps': 'off',
-      'react/prop-types': 'off',
-      'react/display-name': 'off',
-      'react/react-in-jsx-scope': 'off',
-      'no-extra-boolean-cast': 'off',
-      'no-constant-binary-expression': 'off',
-      'no-prototype-builtins': 'off',
-      'no-redeclare': 'off',
-      'no-undef': 'off',
+        "tsdoc/syntax": "off",
 
-      'require-yield': 'off',
-      'no-empty': 'off',
-      'no-control-regex': 'off',
-      'no-useless-escape': 'off',
-      'no-cond-assign': 'off',
-      'no-fallthrough': 'off',
-      'no-func-assign': 'off',
-      'getter-return': 'off',
-      'no-self-assign': 'off',
-      'no-case-declarations': 'off',
-      'no-useless-catch': 'off',
-      'no-unreachable': 'off',
-      'no-unsafe-finally': 'off',
-      'no-misleading-character-class': 'off',
-      'react/no-find-dom-node': 'off',
-      '@typescript-eslint/no-this-alias': 'off',
-      'no-unused-private-class-members': 'off',
-      'no-sparse-arrays': 'off',
-      'valid-typeof': 'off',
-      '@typescript-eslint/no-require-imports': 'off',
-      'no-dupe-class-members': 'off',
-      'no-setter-return': 'off',
-      'react-hooks/rules-of-hooks': 'off',
-      'no-dupe-else-if': 'off',
-      'react/jsx-filename-extension': [
-        1,
-        {
-          extensions: ['.js?', '.jsx', '.ts', '.tsx'],
-        },
-      ],
+        "no-restricted-imports": ["warn", {
+            patterns: [{
+                group: ["@mui/(?!material/styles/createTypography)\\w+/\\w+/\\w+"],
+                message: "Importing from subdirectories is restricted. Use top-level exports where available.",
+            }],
+
+            paths: [{
+                name: "@reduxjs/toolkit",
+                message: "Please do not use Redux Toolkit, migrate to zustand.",
+            }, {
+                name: "react-transition-group",
+                message: "Migrate to MUI or similar. This package is not well maintained.",
+            }],
+        }],
+        '@typescript-eslint/no-unused-vars': 'off',
+        '@typescript-eslint/ban-ts-comment': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-require-imports': 'off',
+        curly: "error",
+        "prefer-const": "error",
+        "no-else-return": "error",
+        "no-unneeded-ternary": "error",
+        "no-alert": "error",
+        "no-empty": "error",
+        'no-loss-of-precision': 'off',
+        'no-extra-boolean-cast': 'off',
+        "no-useless-catch": "error",
+        "require-await": "error",
+        'no-constant-binary-expression': 'off',
+        "no-continue": "error",
+        "react/no-unescaped-entities": "error",
+        "promise/prefer-await-to-then": "error",
+        "react-hooks/exhaustive-deps": "error",
+        "no-console": "error",
+        'no-undef': 'off',
+        "unused-imports/no-unused-imports": "error",
+        "no-magic-numbers": "off",
+        'react/react-in-jsx-scope': 'off',
+        "react/no-direct-mutation-state": "off",
+        'react/prop-types': 'off',
     },
-  },
-]
+}];
